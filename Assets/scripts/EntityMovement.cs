5560b457475fbd3ede9605518a3353cb42de7ad2
Amelia <89955793+lx0919@users.noreply.github.com>
2023-10-31 10:36:29 -0700

    mario

---
+++ Assets/scripts/EntityMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement : MonoBehaviour
+{
+    
+    public float speed = 1f;
+    public Vector2 direction = Vector2.left;
+    public float gravity = -9.81f;
+
+    private Rigidbody2D rb;
+    private Vector2 velocity;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        enabled = false;
+    }
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        rb.WakeUp();
+    }
+
+    private void OnDisable()
+    {
+        rb.velocity = Vector2.zero;
+        rb.Sleep();
+    }
+
+    private void FixedUpdate()
+    {
+        velocity.x = direction.x * speed;
+        velocity.y += Physics2D.gravity.y * Time.fixedDeltaTime;
+
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (rb.Raycast(direction))
+        {
+            direction = -direction;
+        }
+
+        if (rb.Raycast(Vector2.down))
+        {
+            velocity.y = Mathf.Max(velocity.y, 0f);
+        }
+    }
+}
---
+++ Assets/scripts/sidescrolling.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sidescrolling : MonoBehaviour
+{
+
+    private Transform player;
+    public float height = 3f;
+    public float undergroundHeight = -13f;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        transform.position = cameraPosition;
+    }
+
+    public void SetUnderground(bool underground)
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.y = underground ? undergroundHeight : height;
+        transform.position = cameraPosition;
+    }
+}
